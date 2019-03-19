6ed0e2ba106fa51aa37b4f53aad4af2ae79467e7
hecomi <info@hecomi.com>
2019-03-19 22:31:38 +0900

    simplify sender.

---
+++ T265_OculusGo_Sender/Assets/PositionSender/PositionSender.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class PositionSender : MonoBehaviour
+{
+    [SerializeField]
+    uOSC.uOscClient oscClient;
+
+    [SerializeField]
+    Transform target;
+
+    void Update()
+    {
+        var p = target.position;
+        oscClient.Send("/Position", p.x, p.y, p.z);
+    }
+}
