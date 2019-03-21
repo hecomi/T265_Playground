c7f442dd41fc6aeaa0c75099ed1b7d37c992bc31
hecomi <info@hecomi.com>
2019-03-21 16:55:10 +0900

    add calibration.

---
+++ T265_OculusGo_Receiver/Assets/Scripts/TransformReceiver.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+public class TransformReceiver : MonoBehaviour
+{
+    [SerializeField]
+    uOSC.uOscServer oscServer;
+
+    [SerializeField]
+    Transform target;
+
+    [SerializeField]
+    Transform camera;
+
+    Vector3 latestT265Pos = Vector3.zero;
+    Quaternion latestT265Rot = Quaternion.identity;
+    Quaternion calibRot = Quaternion.identity;
+
+    void Start()
+    {
+        oscServer.onDataReceived.AddListener(OnDataReceived);
+    }
+
+    void OnDataReceived(uOSC.Message message)
+    {
+        if (message.address == "/Pos")
+        {
+            var v = message.values;
+            latestT265Pos = new Vector3((float)v[0], (float)v[1], (float)v[2]);
+        }
+        else if (message.address == "/Rot")
+        {
+            var v = message.values;
+            latestT265Rot = new Quaternion((float)v[0], (float)v[1], (float)v[2], (float)v[3]);
+        }
+        else if (message.address == "/Reset")
+        {
+            var yawT265 = latestT265Rot.eulerAngles.y;
+            var yawGo = camera.localEulerAngles.y;
+            calibRot = Quaternion.Euler(0f, yawGo - yawT265, 0f);
+        }
+
+        UpdateTransform();
+    }
+
+    void UpdateTransform()
+    {
+        target.transform.position = calibRot * latestT265Pos;
+    }
+}
---
+++ T265_OculusGo_Sender/Assets/TransformSender/TransformSender.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class TransformSender : MonoBehaviour
+{
+    [SerializeField]
+    uOSC.uOscClient oscClient;
+
+    [SerializeField]
+    Transform target;
+
+    void Update()
+    {
+        var p = target.position;
+        var r = target.rotation;
+        oscClient.Send("/Pos", p.x, p.y, p.z);
+        oscClient.Send("/Rot", r.x, r.y, r.z, r.w);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            oscClient.Send("/Reset");
+        }
+    }
+}
